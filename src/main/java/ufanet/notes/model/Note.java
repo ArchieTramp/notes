67bcccf68b2fd452f2788001f8b7705477507324
Artur <archiesummerrain@gmail.com>
2020-10-27 23:03:08 +0300

    проработка моделей

--- src/main/java/ufanet/notes/model/Note.java
+++ src/main/java/ufanet/notes/model/Note.java
@@ -1,4 +1,26 @@
 package ufanet.notes.model;
 
+import lombok.*;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Data
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@EqualsAndHashCode
+@Table(name = "notes", uniqueConstraints = @UniqueConstraint(columnNames = {"title"}))
 public class Note {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private String title;
+    private Timestamp timestamp;
+    private String note;
+    @ManyToOne
+    @JoinColumn(name = "username")
+    User user;
+
 }
--- src/main/java/ufanet/notes/model/User.java
+++ src/main/java/ufanet/notes/model/User.java
@@ -1,4 +1,25 @@
 package ufanet.notes.model;
 
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Data
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@EqualsAndHashCode
+@Table(name = "users", uniqueConstraints = @UniqueConstraint(columnNames = {"username"}))
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    @OneToMany(mappedBy = "username", fetch = FetchType.EAGER)
+    List<Note> notes;
+
 }
+
